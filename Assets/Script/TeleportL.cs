c05da4fc5af4beda09790794b052565c3f3f11fd
tai <taiyaki1026@icloud.com>
2025-04-07 13:54:36 +0900

    2025/4/7 テレポート途中

---
+++ Assets/Script/TeleportL.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TeleportL : MonoBehaviour
+{
+    // テレポート先の座標
+    public Vector2 teleportPosition = new Vector2(3.75f, 0.15f);
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("teleportL"))
+        {
+            transform.position = teleportPosition;
+        }
+    }
+}
